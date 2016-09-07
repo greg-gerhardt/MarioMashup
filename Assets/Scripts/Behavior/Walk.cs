60d045fda1f2558ca0b50b6ed4474dd493982c53
Gregory Gerhardt <greggerhardt01@gmail.com>
2016-09-06 20:47:25 -0700

    Made sure mario would stop

--- Assets/Scripts/Behavior/Walk.cs
+++ Assets/Scripts/Behavior/Walk.cs
@@ -5,7 +5,6 @@ public class Walk : AbstractBehavior {
 
 	public float speed = 50f;
 
-
 	// Use this for initialization
 	void Start () {
 	
@@ -14,12 +13,12 @@ public class Walk : AbstractBehavior {
 	// Update is called once per frame
 	void Update () {
 
-	
-
 		var right = inputState.GetButtonValue (inputButtons [0]);
 		var left = inputState.GetButtonValue (inputButtons [1]);
-	
 
+		var rightHold = inputState.GetButtonHoldTime (inputButtons [0]);
+		var leftHold = inputState.GetButtonHoldTime (inputButtons [1]);
+	
 		if (right || left) {
 
 			var tmpSpeed = speed;
@@ -27,7 +26,12 @@ public class Walk : AbstractBehavior {
 			var velX = tmpSpeed * (float)inputState.direction;
 
 			body2d.velocity = new Vector2 (velX, body2d.velocity.y);
-		}	
+		}
+
+		//stop mario when no button is pushed
+		if (rightHold == 0 && leftHold == 0) {
+			body2d.velocity = new Vector2 ( 0, body2d.velocity.y);
+		}
 	}
 
 }
