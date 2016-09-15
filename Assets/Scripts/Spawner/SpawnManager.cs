55f931e9981e702d46a94a3b25ceb74f70c147f9
Gregory Gerhardt <greggerhardt01@gmail.com>
2016-09-14 22:13:58 -0700

    Idk breh

--- Assets/Scripts/Spawner/SpawnManager.cs
+++ Assets/Scripts/Spawner/SpawnManager.cs
@@ -5,23 +5,39 @@ public class SpawnManager : MonoBehaviour {
 	public GameObject[] Enemy;
 	public float SpawnTimer;
 
+	public GameObject[] SpawnedEnemies;
+	private int i;
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine ("SpawnEnemy");
+		i = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		CheckArray ();
 	}
 
 	IEnumerator SpawnEnemy(){
 		yield return new WaitForSeconds (Random.value * SpawnTimer);
-		Instantiate (Enemy [0], transform.position, Quaternion.identity);
+		if (SpawnedEnemies.Length < 5) {
+			SpawnedEnemies  += Instantiate (Enemy [0], transform.position, Quaternion.identity) as GameObject;
+		}
 		StartCoroutine ("RecurseSpawn");
 	}
 
 	IEnumerator RecurseSpawn(){
 		yield return StartCoroutine ("SpawnEnemy");
+	}
+
+	void CheckArray (){
+		int k = 0;
+		foreach (GameObject Spawned in SpawnedEnemies){
+			k++;
+		}
+		if (k < SpawnedEnemies.Length) {
+			i = k;
+		}
 	}
 }
